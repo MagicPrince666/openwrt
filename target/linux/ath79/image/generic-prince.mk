506a2b004423ffe20070271b2896098af31761b6
MagicPrince666 <846863428@qq.com>
2020-07-22 15:42:46 +0800

    add my mechine

---
+++ target/linux/ath79/image/generic-prince.mk
@@ -0,0 +1,27 @@
+include ./common-buffalo.mk
+include ./common-netgear.mk
+include ./common-yuncore.mk
+
+DEVICE_VARS += ADDPATTERN_ID ADDPATTERN_VERSION
+DEVICE_VARS += SEAMA_SIGNATURE SEAMA_MTDBLOCK
+DEVICE_VARS += KERNEL_INITRAMFS_PREFIX
+
+define Device/prince_link
+  ATH_SOC := ar9342
+  DEVICE_TITLE := Prince link 16M
+  IMAGE_SIZE := 16128k
+  DEVICE_PACKAGES := kmod-usb-core kmod-usb2 kmod-usb-acm arptables \
+  kmod-usb-net kmod-usb-net-asix luci kmod-usb-net-rtl8152 \
+  kmod-usb-net-rndis atftp wpad-mesh-openssl libstdcpp default-settings
+endef
+TARGET_DEVICES += prince_link
+
+define Device/prince_link-8m
+  ATH_SOC := ar9342
+  DEVICE_TITLE := prince link 8M
+  IMAGE_SIZE := 7936k
+  DEVICE_PACKAGES := kmod-usb-core kmod-usb2 kmod-usb-acm arptables \
+  kmod-usb-net kmod-usb-net-asix luci kmod-usb-net-rtl8152 \
+  kmod-usb-net-rndis atftp wpad-mesh-openssl libstdcpp usbutils default-settings
+endef
+TARGET_DEVICES += prince_link-8m
