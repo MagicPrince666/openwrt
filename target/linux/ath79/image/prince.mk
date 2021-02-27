97fcec02904e11c6cd398b25b9b9d7d7ceed81ba
3700x <3700x@3700xdeiMac-Pro.lan>
2021-02-27 15:17:29 +0800

    fix ath

--- target/linux/ath79/image/prince.mk
+++ target/linux/ath79/image/prince.mk
@@ -1,11 +1,3 @@
-include ./common-buffalo.mk
-include ./common-netgear.mk
-include ./common-yuncore.mk
-
-DEVICE_VARS += ADDPATTERN_ID ADDPATTERN_VERSION
-DEVICE_VARS += SEAMA_SIGNATURE SEAMA_MTDBLOCK
-DEVICE_VARS += KERNEL_INITRAMFS_PREFIX
-
 define Device/prince_link
   SOC := ar9342
   DEVICE_TITLE := prince link 16M
@@ -17,7 +9,7 @@ endef
 TARGET_DEVICES += prince_link
 
 define Device/prince_link-8m
-  ATH_SOC := ar9342
+  SOC := ar9342
   DEVICE_TITLE := prince link 8M
   IMAGE_SIZE := 7936k
   DEVICE_PACKAGES :=  arptables luci atftp wpad-mesh-openssl libstdcpp usbutils
